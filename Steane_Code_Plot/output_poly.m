1eca47f542b86d0efaf05b0cbd1ea7cb8018fef1
lemar1998 <39720809+lemar1998@users.noreply.github.com>
2019-06-26 17:59:13 +0800

    limits the order of txt output to 8
    achieved with MuPAD function mtaylor, which takes taylor series approximation to a multivariate symbolic expression up to a specified order

--- Steane_Code_Plot/output_poly.m
+++ Steane_Code_Plot/output_poly.m
@@ -1,6 +1,8 @@
 
+
 p_m = sym('p_m','real');
 p_q = sym('p_q','real');
+
 S_QEC = Sqec3times(p_m, p_q);
 S_DBR = SrseOnce_full(p_m, p_q);
 S_MR = SrseOnce_one(p_m, p_q);
@@ -8,6 +10,12 @@ S_MR = SrseOnce_one(p_m, p_q);
 F_QEC = expand(1-S_QEC);
 F_DBR = expand(1-S_DBR);
 F_MR = expand(1-S_MR);
+%% this section limits the order of the polynomials to 8, using MuPAD function mtaylor
+F_QEC = feval(symengine,'mtaylor',F_QEC, '[p_m, p_q]', 'AbsoluteOrder = 9');
+F_DBR = feval(symengine,'mtaylor',F_DBR, '[p_m, p_q]', 'AbsoluteOrder = 9');
+F_MR = feval(symengine,'mtaylor',F_MR, '[p_m, p_q]', 'AbsoluteOrder = 9');
+%% 
+
 file = fopen('polynomials_Steane_Code.txt','w');
 fprintf(file,'F_QEC = %s\n',F_QEC);
 fprintf(file,'F_DBR = %s\n',F_DBR);
--- bitflip_code/output_poly.m
+++ bitflip_code/output_poly.m
@@ -1,12 +1,20 @@
 
+
 p_m = sym('p_m','real');
 p_q = sym('p_q','real');
+
 S_QEC = Sqec3times(p_m, p_q);
 S_DBR = SrseOnce(p_m, p_q);
 
 
 F_QEC = expand(1-S_QEC);
 F_DBR = expand(1-S_DBR);
+
+%% this section limits the order of the polynomials to 8, using MuPAD function mtaylor
+F_QEC = feval(symengine,'mtaylor',F_QEC, '[p_m, p_q]', 'AbsoluteOrder = 9');
+F_DBR = feval(symengine,'mtaylor',F_DBR, '[p_m, p_q]', 'AbsoluteOrder = 9');
+
+%% 
 
 file = fopen('polynomials_Bitflip_Code.txt','w');
 fprintf(file,'F_QEC = %s\n',F_QEC);
